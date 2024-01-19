8ca5a0af5687740768a5ee29b5456dfc4038e396
Robert Dukes <RDukes@scpdist.com>
2024-01-19 10:33:39 -0500

    Pushing forward some animation changes

--- Main/game.py
+++ Main/game.py
@@ -2,7 +2,7 @@ import sys
 
 import pygame
 
-from scripts.utils import load_image, load_images
+from scripts.utils import load_image, load_images, Animation
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
@@ -27,8 +27,13 @@ class Game:
             'player': load_image('entities/player.png'),
             'background': load_image('background.png'),
             'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur = 6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur = 4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
         }
-        
+
         self.clouds = Clouds(self.assets['clouds'], count=16)
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
